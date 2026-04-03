450e39cd51684d129e0bdbf79543e8e058e924bc
MohamedAEleanany <mohamedeleanany7@gmail.com>
2026-04-03 15:38:40 +0200

    Route redirect view

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ Route::get('/test',function(){
    dd('Heeloo');
 });
 
-
+//go to controller to function that return die and dump
 Route::get('test1', [TestController::class,'test']);
-
+//go to controller to function that return view
 Route::get('testview', [TestController::class , 'testview']);
+//short hand route for goto view 
+Route::view('testview1', 'test');
+//redirect for go to specfic destintation
+Route::redirect('test2','/testview1');
